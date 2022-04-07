337d30b744d565d3bdc3b68fe7deaa41ad614ac8
Marcel <marcel.bernet@ch-open.ch>
2022-04-07 10:34:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 
 module "lerncloud" {
+  source     = "git::https://github.com/mc-b/terraform_lerncloud_multipass.git"      
   #source     = "git::https://github.com/mc-b/terraform_lerncloud_aws.git"
   #source     = "git::https://github.com/mc-b/terraform_lerncloud_azure.git"
   #source     = "git::https://github.com/mc-b/terraform_lerncloud_maas.git"
