f770f0b1ed0527cafad3f3c6d8d6d46f5b4c9d49
Marcel <marcel.bernet@ch-open.ch>
2022-04-14 11:34:02 +0200

    Update main.tf
    Ohne .git einfacher zum Testen

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 
 module "lerncloud" {
-  source     = "git::https://github.com/mc-b/terraform_lerncloud_multipass.git"      
-  #source     = "git::https://github.com/mc-b/terraform_lerncloud_aws.git"
-  #source     = "git::https://github.com/mc-b/terraform_lerncloud_azure.git"
-  #source     = "git::https://github.com/mc-b/terraform_lerncloud_maas.git"
+  source     = "git::https://github.com/mc-b/terraform_lerncloud_multipass"      
+  #source     = "git::https://github.com/mc-b/terraform_lerncloud_aws"
+  #source     = "git::https://github.com/mc-b/terraform_lerncloud_azure"
+  #source     = "git::https://github.com/mc-b/terraform_lerncloud_maas"
   module     = "m122"
   userdata   = "cloud-init.yaml"
 }
